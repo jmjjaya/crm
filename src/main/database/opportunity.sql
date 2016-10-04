1c9ce74e4166e2852250225e6600de03b31bc17e
jeewan-jayathilake <efacjeewan@gmail.com>
2016-10-05 00:18:05 +0530

    Modified table creation sql added customer model class and table create file

--- src/main/database/opportunity.sql
+++ src/main/database/opportunity.sql
@@ -1,3 +1,17 @@
+/******************************
+** File:  opportunity.sql  
+** Desc:  This will create the Opportunity table and this is used only in fresh installations
+*		  This file will be called from crm.create. This file should be updated when a new field is
+*		  added.
+** Auth:  Jeewan Jayathilake
+**************************
+** Change History
+**************************
+**  Date	    Author    Description	
+**  --------    -------   ------------------------------------
+**  2016/10/01  JeeJ      created.
+*******************************/
+
 CREATE TABLE IF NOT EXISTS `JEEJ`.`OPPORTUNITY_TAB` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `description` varchar(20) NOT NULL DEFAULT '',
